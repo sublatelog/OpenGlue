6bc470795f78d8c19e15097792efd9f330b798a4
sublatelog <blueganz@gmail.com>
2022-05-03 20:58:54 +0900

    Update misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -50,7 +50,7 @@ def get_inverse_transformation(transformation):
         
         print("R_t")
         print(R_t)
-        print(T.unsqueeze(-1))
+        print(T)
         
         return {
             'type': transformation['type'],
