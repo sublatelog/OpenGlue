ace762e0b7a5380fb6facc04fb757b7cd45785da
sublatelog <blueganz@gmail.com>
2022-05-03 22:44:41 +0900

    Update misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -99,16 +99,13 @@ def reproject_3d(kpts, K0, K1, T, R, depth0, eps=1e-8):
         ]
     mask = ~torch.isclose(depth, depth.new_tensor(0.0))  # mask for values with missing depth information
     
-    print("kpts_transformed")
-    print(kpts_transformed.shape)
-    print(depth.unsqueeze(-1).shape)
-    
     # multiply by corresponding depth  
     kpts_transformed = kpts_transformed * depth
 #     kpts_transformed = kpts_transformed * depth.unsqueeze(-1)
     
     # apply (R, T)
-    kpts_transformed = torch.matmul(kpts_transformed, R_t) + T.unsqueeze(1)
+    kpts_transformed = torch.matmul(kpts_transformed, R_t) + T
+#     kpts_transformed = torch.matmul(kpts_transformed, R_t) + T.unsqueeze(1)
     kpts_transformed = torch.matmul(kpts_transformed, K1_t)
     
 
