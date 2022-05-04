6e91325f0a32ff8651166a11be422b8ab6617bdb
sublatelog <blueganz@gmail.com>
2022-05-04 21:03:48 +0900

    Update __init__.py

--- models/features/__init__.py
+++ models/features/__init__.py
@@ -18,8 +18,7 @@ def get_feature_extractor(model_name):
     Create method form configuration
     """
     if model_name not in methods:
-        raise NameError('{} module was not found among local descriptors. Please choose one of the following '
-                        'methods: {}'.format(model_name, ', '.join(methods.keys())))
+        raise NameError('{} module was not found among local descriptors. Please choose one of the following methods: {}'.format(model_name, ', '.join(methods.keys())))
 
     return methods[model_name]
 
