8e2dc5d289797644c1aee497a0730f443aef370e
sublatelog <blueganz@gmail.com>
2022-05-04 12:09:54 +0900

    Update misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -102,21 +102,25 @@ def reproject_3d(kpts, K0, K1, T, R, depth0, eps=1e-8):
     
     
     # multiply by corresponding depth  
-#     kpts_transformed = kpts_transformed * depth
+    
+    # torch.Size([2, 604, 3]) * torch.Size([2, 604, 3])
+    kpts_transformed = kpts_transformed * depth
 
     # torch.Size([2, 604, 3]) * torch.Size([2, 604, 3, 1])
 #     kpts_transformed = kpts_transformed * depth.unsqueeze(-1)
     
     # torch.Size([2, 604, 3, 1]) * torch.Size([2, 604, 3, 1])
-    kpts_transformed = kpts_transformed.unsqueeze(-1) * depth.unsqueeze(-1)
+#     kpts_transformed = kpts_transformed.unsqueeze(-1) * depth.unsqueeze(-1)
     
 
     print(kpts_transformed.shape)
     print(R_t.shape)
     
     # apply (R, T)
-    # 
+    # torch.Size([2, 604, 3, 1]) * torch.Size([2, 3, 3]) 
     kpts_transformed = torch.matmul(kpts_transformed, R_t)   
+    
+    
     kpts_transformed = kpts_transformed.unsqueeze(-1) + T.unsqueeze(1)
 #     kpts_transformed = torch.matmul(kpts_transformed, R_t) + T.unsqueeze(1)
 
